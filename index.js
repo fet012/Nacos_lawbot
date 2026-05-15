733bd89ea023767e1340d1e1a4fc039d28d4c5ad
fet012 <falolachristiana446@gmail.com>
2026-05-15 07:56:33 +0100

    fix session  cross-origin auth

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const corsOptions = {
   optionsSuccessStatus: 200
 };
 app.use(cors(corsOptions));
-app.options('*', cors(corsOptions));
+app.options('/*', cors());
 app.use(express.json());
 
 // Session (no MongoDB store for now)
